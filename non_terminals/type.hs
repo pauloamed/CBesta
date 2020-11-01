c480204ce6a0ac063244583516e835d832e3bd5b
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-11-01 18:45:16 -0300

    type-related parsers

---
+++ non_terminals/type.hs
@@ -0,0 +1,62 @@
+module TypeGrammar where
+
+import Lexer
+import Text.Parsec
+
+import FlowPrimTokens
+import LiteralsPrimTokens
+import MainPrimTokens
+import OperatorsPrimTokens
+import ScopesPrimTokens
+
+
+-- <type> -> POINTER <enclosed_type>
+--           | LIST <enclosed_type>
+--           | ARRAY <enclosed_type>
+--           | HASHMAP LESS_THAN <type> COMMA <type> GREATER_THAN
+--           | TUPLE LESS_THAN <types> GREATER_THAN
+--           | <simple_type>
+--           | ID
+typeParser :: Parsec [Token] st [Token]
+typeParser = (do  x <- pointerToken <|> listToken <|> arrayToken
+                  enclosedType <- enclosedTypeParser
+                  return x:enclosedType) <|>
+             (do  idd <- idToken
+                  return ([idd])) <|>
+             (do  simpleType <- simpleTypeToken
+                  return simpleType) <|>
+             (do  hashmap <- hashmapToken
+                  lessThan <- lessThanToken
+                  typee1 <- typeParser
+                  comma <- commaToken
+                  typee2 <- typeParser
+                  greaterThan <- greaterThanToken
+                  return (hashmap:lessThan:typee1 ++ comma:typee2 ++ greaterThan)) <|>
+             (do  tuple <- tupleToken
+                  lessThan <- lessThanToken
+                  types <- typesParser
+                  greaterThan <- greaterThanToken
+                  return (tuple:lessThan:types ++ [greaterThan]))
+
+
+-- <enclosed_type> -> LESS_THAN <type> GREATE_THAN
+enclosedTypeParser :: Parsec [Token] st [Token]
+enclosedTypeParser = (do  lessThan <- lessThanToken
+                          typee <- typeParser
+                          greaterThan <- greaterThanToken
+                          return lessThan:typee ++ [greaterThan])
+
+
+-- <types> -> <type> { COMMA <type> }
+typesParser :: Parsec [Token] st [Token]
+typesParser = (do   typee <- typeParser
+                    remeaning <- many (do   comma <- commaToken
+                                            typee <- typeParser
+                                            return comma:typee)
+                    return (typee ++ concat(remeaning)))
+
+
+-- <simple_type> -> INT | BOOL | DOUBLE | STRING
+simpleTypeParser :: Parsec [Token] st [Token]
+simpleTypeParser = (do  x <- intToken <|> boolToken <|> doubleToken <|> stringToken
+                        return x)
