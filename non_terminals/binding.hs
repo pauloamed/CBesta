919ce03edae6c474dad3b675feca659d7114c074
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-11-01 19:02:31 -0300

    added binding module, non cyclic dependencies

---
+++ non_terminals/binding.hs
@@ -0,0 +1,93 @@
+module BindingGrammar where
+
+import Lexer
+import Text.Parsec
+
+import FlowPrimTokens
+import LiteralsPrimTokens
+import MainPrimTokens
+import OperatorsPrimTokens
+import ScopesPrimTokens
+
+
+-- <var_binding> -> <assignments> | <declrs>
+varBindingParser :: Parsec [Token] st [Token]
+varBindingParser = (do  x <- assignmentsParser <|> declrsParser
+                        return x)
+
+
+-- <declrs> -> <type> ID <assign_expr> { COMMA ID <assign_expr>}
+declrsParser :: Parsec [Token] st [Token]
+declrsParser = (do  typee <- typeParser
+                    idd <- idToken
+                    assignExpr <- assignExprParser
+                    remeaning <- (do  comma <- commaToken
+                                      idd <- idToken
+                                      assignExpr <- assignExprParser
+                                      return comma:idd:assignExpr)
+                    return typee ++ idd:assignExpr ++ concat(remeaning))
+
+
+-- <assign_expr> -> ASSIGN <expr>
+assignExprParser :: Parsec [Token] st [Token]
+assignExprParser = (do  assign <- assignParser
+                        expr <- exprParser
+                        return assign:expr)
+
+
+-- <assignments> -> <assignment> { COMMA <assignment> }
+assignmentsParser :: Parsec [Token] st [Token]
+assignmentsParser = (do   assignment <- assignmentParser
+                          remeaning <- many (do   comma <- commaToken
+                                                  assignment <- assignmentParser
+                                                  return comma:assignment)
+                          return assignment ++ remeaning)
+
+
+-- <assignments_op> -> <assign_expr> { COMMA <assignment> }
+assignmentsOpParser :: Parsec [Token] st [Token]
+assignmentsOpParser = (do   assignExpr <- assignExprParser
+                            remeaning <- many (do   comma <- commaToken
+                                                    assignment <- assignmentParser
+                                                    return comma:assignment)
+                            return assignExpr ++ concat(remeaning))
+
+
+-- <assignment> -> ID <assign_expr> | <deref_pointer> <assign_expr>
+assignmentParser :: Parsec [Token] st [Token]
+assignmentParser = (do  idd <- idToken
+                        assignExpr <- assignExprParser
+                        return idd:assignExpr) <|>
+                   (do  derefPointer <- derefPointerParser
+                        assignExpr <- assignExprParser
+                        return derefPointer ++ assignExpr)
+
+
+-- <void_command> -> <free> | <print> | <read>
+voidCommandParser :: Parsec [Token] st [Token]
+voidCommandParser = (do   x <- freeParser <|> printParser <|> readParser
+                          return x)
+
+
+-- <free> -> FREE <expr>
+freeParser :: Parsec [Token] st [Token]
+freeParser = (do  free <- freeToken
+                  expr <- exprParser
+                  return free:expr)
+
+-- <print> -> PRINT LEFT_PAREN <expr> RIGHT_PAREN
+printParser :: Parsec [Token] st [Token]
+printParser = (do printt <- printToken
+                  leftParen <- leftParenToken
+                  expr <- exprParser
+                  rightParen <- rightParenToken
+                  return printt:leftParen:expr ++ [rightParen])
+
+
+-- <read> -> READ LEFT_PAREN ID RIGHT_PAREN
+readParser :: Parsec [Token] st [Token]
+readParser = (do  readd <- readToken
+                  leftParen <- leftParenToken
+                  idd <- idToken
+                  rightParen <- rightParenToken
+                  return readd:leftParen:idd:[rightParen])
